f128a26e7586c832b5ce886311ca6995e43c13b7
higashiyama-shunya <higashiyama.shunya@oysterworld.jp>
2023-06-14 17:17:12 +0900

    テスト作成
    練習問題のコインケースとアニマルクラスを継承したdogクラス、catクラスのテストの作成

---
+++ kitakoubou7/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using kitakoubou7;
+using kitakoubou8;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod,TestCategory("カテゴリー1")]
+        public void AddCoinsTest()
+        {
+            //AddCoinsとGetAmountがちゃんと動作するかテスト
+            CoinCase cc = new CoinCase();
+            cc.AddCoins(500, 10);
+            cc.AddCoins(100, 10);
+            cc.AddCoins(50, 10);
+            cc.AddCoins(10, 10);
+            cc.AddCoins(5, 10);
+            cc.AddCoins(1, 10);
+            cc.AddCoins(48, 10);
+
+            int testnum = (500 + 100 + 50 + 10 + 5 + 1) * 10;
+
+            Assert.AreEqual(cc.GetAmount(), testnum);
+        }
+        [TestMethod, TestCategory("カテゴリー1")]
+        public void AddCoinsTestNum()
+        {
+            //AddCoinsとGetAmountがちゃんと動作するかテスト Amount引数ありver
+            CoinCase cc = new CoinCase();
+            cc.AddCoins(500, 10);
+            cc.AddCoins(100, 10);
+            cc.AddCoins(50, 10);
+            cc.AddCoins(10, 10);
+            cc.AddCoins(5, 10);
+            cc.AddCoins(1, 10);
+            cc.AddCoins(48, 10);
+
+            Assert.AreEqual(cc.GetAmount(500), 5000);
+        }
+
+        [TestMethod, TestCategory("カテゴリー2")]
+        public void GetCount()
+        {
+            //AddCoinsとGetCountがちゃんと動作するかテスト
+            CoinCase cc = new CoinCase();
+            cc.AddCoins(500, 10);
+            cc.AddCoins(100, 10);
+            cc.AddCoins(50, 10);
+            cc.AddCoins(10, 10);
+            cc.AddCoins(5, 10);
+            cc.AddCoins(1, 10);
+            cc.AddCoins(48, 10);
+
+            Assert.AreEqual(cc.GetCount(), 60);
+        }
+
+        [TestMethod, TestCategory("カテゴリー2")]
+        public void GetCountNum()
+        {
+            //AddCoinsとGetCountがちゃんと動作するかテスト Count引数ありver
+            CoinCase cc = new CoinCase();
+            cc.AddCoins(500, 10);
+            cc.AddCoins(100, 10);
+            cc.AddCoins(50, 10);
+            cc.AddCoins(10, 10);
+            cc.AddCoins(5, 10);
+            cc.AddCoins(1, 10);
+            cc.AddCoins(48, 10);
+
+            Assert.AreEqual(cc.GetCount(500), 10);
+        }
+    }
+}
---
+++ kitakoubou7/UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using kitakoubou8;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod,TestCategory("カテゴリー1")]
+        public void DogCatAbility()
+        {
+            //それぞれ固有のメソッドを実行して正しく動くか
+            Dog dog = new Dog("タロウ",5);
+            Cat cat = new Cat("コマ",4);
+
+
+            Assert.AreEqual(dog.Run(), "トコトコ");
+            Assert.AreEqual(cat.Sleep(), "スースー");
+        }
+
+        [TestMethod, TestCategory("カテゴリー2")]
+        public void DogCatSpeak()
+        {
+            //Speakメソッドでそれぞれ違う結果を返しているか
+            Dog dog = new Dog("タロウ", 5);
+            Cat cat = new Cat("コマ", 4);
+
+            Assert.AreEqual(dog.Speak(), "ワンワン");
+            Assert.AreEqual(cat.Speak(), "ニャー");
+        }
+
+        [TestMethod, TestCategory("カテゴリー3")]
+        public void DogCatNameAge()
+        {
+            Dog dog = new Dog("タロウ", 5);
+            Cat cat = new Cat("コマ", 4);
+
+            Assert.AreEqual(dog.Name,"タロウ");
+            Assert.AreEqual(dog.Age, 5);
+            Assert.AreEqual(cat.Name, "コマ");
+            Assert.AreEqual(cat.Age, 4);
+
+        }
+    }
+}
